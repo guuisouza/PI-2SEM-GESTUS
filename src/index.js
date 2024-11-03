fc48277c05b2a2095fef4ea93c8a2bb7a109aaba
Olie-dc <ellora.oliverio@fatec.sp.gov.br>
2024-11-03 13:30:08 -0300

    Implementando rotas no index.js

--- src/index.js
+++ src/index.js
@@ -1,20 +1,25 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const path = require('path')
 
 const app = express()
 
 const connection = require('./db/connection')
 
 // Models - Importe aqui os models das tabelas criadas e descomente ao rodar a primeira vez
-// const Funcionario = require('./models/Funcionario')
+const Funcionario = require('./models/Funcionario')
+const Finance = require('./models/Finance')
 
 // import routes
+const financeRoutes = require('./routes/financeRoutes')
+const authRoutes = require('./routes/authRoutes')
 
 // controllers
 
 // template engine
 app.engine('handlebars', exphbs.engine())
 app.set('view engine', 'handlebars')
+app.set('views', path.join(__dirname, 'views'))
 
 // receber resposta do body
 app.use(express.urlencoded({ extended: true }))
@@ -22,11 +27,15 @@ app.use(express.json())
 
 app.use(express.static('public'))
 
+//Rota
+app.use('/finance', financeRoutes)
+app.use('/', authRoutes)
+
 // use routes
 
 connection
-  // .sync({ alter: true }) // quando for rodar a primeira vez a sua tabela descomente essa e comente a de baixo
-  .sync()
+  .sync({ alter: true }) // quando for rodar a primeira vez a sua tabela descomente essa e comente a de baixo
+  //.sync()
   .then(() => {
     app.listen(3000)
   })
